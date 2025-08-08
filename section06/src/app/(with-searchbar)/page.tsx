7aea4a92155a8aa59105c08fc670ab0230593dbb
YoungKyuK <dudrb7795@gmail.com>
2025-08-08 16:04:07 +0900

    Streaming

---
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,81 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+
+// export const dynamic = "force-dynamic";
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정
+// 4. error : 페이지를 강제로 Static 페이지 설정( 설정하면 안되는 이유 -> 빌드 오류 발생)
+
+// 데이터 캐시 : fetch 메서드에서만 활용 가능
+async function AllBooks() {
+  await delay(1500);
+  // 비동기로 서버컴포넌트의 api 호출
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "force-cache" }
+  );
+
+  // 에러 핸들링
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoBooks() {
+  await delay(3000);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    throw new Error("데이터를 불러오는 중 오류가 발생했습니다.");
+  }
+
+  const RecoBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {RecoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+// 강제로 dynamic 페이지로 설정
+export const dynamic = "force-dynamic";
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+
+        <Suspense fallback={<BookListSkeleton count={3} />}>
+          <RecoBooks />
+        </Suspense>
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <Suspense fallback={<BookListSkeleton count={10} />}>
+          <AllBooks />
+        </Suspense>
+      </section>
+    </div>
+  );
+}
