bc4a873c8f4f2894095d3e7270cff0c07eb43103
YoungKyuK <dudrb7795@gmail.com>
2025-08-12 18:34:04 +0900

    vercel

---
+++ section09/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  // consolg의 로그 설정
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    domains: ["shopping-phinf.pstatic.net"],
+  },
+};
+
+export default nextConfig;
